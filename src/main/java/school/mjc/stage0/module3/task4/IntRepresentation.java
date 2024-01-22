7c9ec9e9334f590b1cd6452f8803fe8499b6bc98
niluwka <ninimamatova0403@gmail.com>
2024-01-22 15:25:54 +0500

    Primitives

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r' ;
     }
 }
