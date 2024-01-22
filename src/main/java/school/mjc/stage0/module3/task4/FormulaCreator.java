f678a7372a6317c66a8313fd43874dd9e45ff34c
nilufar <ninimamatova0403@gmail.com>
2024-01-22 17:04:10 +0500

    55

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-     int a;
-     int b ;
+     int a = 1;
+     int b = 1 ;
      int x = ((9*a*2) - (5*b) + 2 + a - 7) *((a+b-4*a*b)/2) ;
     }
 }
